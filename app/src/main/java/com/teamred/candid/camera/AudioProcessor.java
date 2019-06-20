11596ccf568c3623a125a6ab67721973967f7f5b
Akash <Akash@v1040-wn-rt-c-11-207.campus-dynamic.uwaterloo.ca>
2019-06-20 16:58:46 -0400

    added initial code for mic

--- app/src/main/java/com/teamred/candid/camera/AudioProcessor.java
+++ app/src/main/java/com/teamred/candid/camera/AudioProcessor.java
@@ -3,6 +3,7 @@ package com.teamred.candid.camera;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,7 +11,64 @@ import io.reactivex.Observable;
 import io.reactivex.subjects.PublishSubject;
 
 public class AudioProcessor {
+    private static final int SAMPLE_RATE_HZ = 8000; // Use 44.1kHz for actual devices
+    private static final long SAMPLE_PERIOD_MS = (long) (1000 * 1.0 / (double) SAMPLE_RATE_HZ);
 
+    private MediaRecorder Recorder;
+    private AudioProcessor processor;
+
+    public Observable<Double> audioStream() {
+        recorderStart();
+
+        return Observable
+                .interval(SAMPLE_PERIOD_MS, TimeUnit.MILLISECONDS)
+                .map(i -> recorderGetLevel());
+    }
+
+    public void recorderStart() {
+
+        if (Recorder == null) {
+            Recorder = new MediaRecorder();
+            Recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            Recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            Recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            Recorder.setOutputFile("/dev/null");
+
+        }
+
+        try {
+            Recorder.prepare();
+        } catch (java.io.IOException ioe) {
+            Log.e("mic", "prepare error");
+        }
+
+        try {
+            Recorder.start();
+        } catch (java.lang.SecurityException e) {
+            Log.e("mic", "start error");
+        }
+    }
+
+    public void recorderStop() {
+        if (Recorder != null) {
+            Recorder.stop();
+            Recorder.release();
+            Recorder = null;
+        }
+    }
+
+    public double recorderGetLevel(){
+        if (Recorder != null){
+            return Recorder.getMaxAmplitude();
+        }
+        else{
+            return 0;
+        }
+    }
+}
+
+
+/*
     private static final int SAMPLE_RATE_HZ = 8000; // Use 44.1kHz for actual devices
     private static final long SAMPLE_PERIOD_MS = (long) (1000 * 1.0 / (double) SAMPLE_RATE_HZ);
 
@@ -44,5 +102,6 @@ public class AudioProcessor {
             max = Math.max(max, Math.abs(s));
         }
         return max;
-    }
-}
+    }*/
+
+//}
