9bc5502d47d57f0aaf5e6816ab0164880cf5d3c1
Priyesh Patel <p99patel@uwaterloo.ca>
2019-06-22 16:59:33 -0400

    cleanup

--- app/src/main/java/com/teamred/candid/camera/AudioProcessor.java
+++ app/src/main/java/com/teamred/candid/camera/AudioProcessor.java
@@ -1,21 +1,17 @@
 package com.teamred.candid.camera;
 
-import android.media.AudioFormat;
-import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
-import io.reactivex.subjects.PublishSubject;
 
 public class AudioProcessor {
     private static final int SAMPLE_RATE_HZ = 8000; // Use 44.1kHz for actual devices
     private static final long SAMPLE_PERIOD_MS = (long) (1000 * 1.0 / (double) SAMPLE_RATE_HZ);
 
-    private MediaRecorder Recorder;
-    private AudioProcessor processor;
+    private MediaRecorder recorder;
 
     public Observable<Double> audioStream() {
         recorderStart();
@@ -25,83 +21,39 @@ public class AudioProcessor {
                 .map(i -> recorderGetLevel());
     }
 
-    public void recorderStart() {
+    private void recorderStart() {
 
-        if (Recorder == null) {
-            Recorder = new MediaRecorder();
-            Recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            Recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            Recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            Recorder.setOutputFile("/dev/null");
+        if (recorder == null) {
+            recorder = new MediaRecorder();
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setOutputFile("/dev/null");
 
         }
 
         try {
-            Recorder.prepare();
+            recorder.prepare();
         } catch (java.io.IOException ioe) {
             Log.e("mic", "prepare error");
         }
 
         try {
-            Recorder.start();
+            recorder.start();
         } catch (java.lang.SecurityException e) {
             Log.e("mic", "start error");
         }
     }
 
     public void recorderStop() {
-        if (Recorder != null) {
-            Recorder.stop();
-            Recorder.release();
-            Recorder = null;
+        if (recorder != null) {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
         }
     }
 
-    public double recorderGetLevel(){
-        if (Recorder != null){
-            return Recorder.getMaxAmplitude();
-        }
-        else{
-            return 0;
-        }
+    private double recorderGetLevel() {
+        return recorder != null ? recorder.getMaxAmplitude() : 0;
     }
 }
-
-
-/*
-    private static final int SAMPLE_RATE_HZ = 8000; // Use 44.1kHz for actual devices
-    private static final long SAMPLE_PERIOD_MS = (long) (1000 * 1.0 / (double) SAMPLE_RATE_HZ);
-
-    private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(
-            SAMPLE_RATE_HZ, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-
-    private final AudioRecord audio;
-
-    public AudioProcessor() {
-        audio = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE_HZ,
-                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE);
-    }
-
-    public Observable<Double> audioStream() {
-        // TODO: Actually use the recorder
-//        audio.startRecording();
-        return Observable
-                .interval(SAMPLE_PERIOD_MS, TimeUnit.MILLISECONDS)
-                .map(i -> 0.0);
-    }
-
-    public void stop() {
-        audio.stop();
-    }
-
-    private double getAmplitude() {
-        byte[] buffer = new byte[BUFFER_SIZE];
-        audio.read(buffer, 0, BUFFER_SIZE);
-        int max = 0;
-        for (short s : buffer) {
-            max = Math.max(max, Math.abs(s));
-        }
-        return max;
-    }*/
-
-//}
