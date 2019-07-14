a2ddea712020d3b3851ca57ac92426e7ff1f3b9d
Akash Kalimili <akalimil@uwaterloo.ca>
2019-07-14 18:19:49 -0400

    added stuuff

--- app/src/main/java/com/teamred/candid/ui/MainActivity.java
+++ app/src/main/java/com/teamred/candid/ui/MainActivity.java
@@ -9,20 +9,50 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
+import com.google.api.client.auth.oauth.OAuthGetAccessToken;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.Facing;
 import com.otaliastudios.cameraview.Flash;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 import com.teamred.candid.camera.AudioProcessor;
 import com.teamred.candid.camera.BitmapProcessor;
 import com.teamred.candid.R;
 import com.teamred.candid.data.SessionManager;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "CandidMain";
+    private static final int RC_SIGN_IN = 512;
 
     private Disposable dispose;
     private CameraView cameraView;
@@ -33,12 +63,97 @@ public class MainActivity extends AppCompatActivity {
     private View overlay;
 
     private boolean sessionInProgress = false;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                handleSignInResult(task);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) throws ApiException {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String idToken = account.getIdToken();
+            String authCode = account.getServerAuthCode();
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("grant_type", "authorization_code")
+                .add("client_id", "1006666980865-tu83mb86ku1et8ccikrq7ajdagl4ohdp.apps.googleusercontent.com")
+                .add("client_secret", "GNaSkd6yT73PhM6xN_eUnox-")
+                .add("redirect_uri","")
+                .add("code",authCode )
+                .add("id_token", idToken)
+                .build();
+        final Request request = new Request.Builder()
+                .url("https://www.googleapis.com/oauth2/v4/token")
+                .post(requestBody)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Request request, final IOException e) {
+                Log.e("a", e.toString());
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    final String message = jsonObject.toString(5);
+                    Log.i("a", message);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .requestScopes(new Scope("https://www.googleapis.com/auth/photoslibrary")	)
+                .requestIdToken("1006666980865-tu83mb86ku1et8ccikrq7ajdagl4ohdp.apps.googleusercontent.com")
+                .requestServerAuthCode("1006666980865-tu83mb86ku1et8ccikrq7ajdagl4ohdp.apps.googleusercontent.com", true)
+                .build();
+
+
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(v -> {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        });
+
+    /*Add after
+    // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+updateUI(account);
+     */
+
 
         cameraView = findViewById(R.id.camera);
         cameraView.setLifecycleOwner(this);
